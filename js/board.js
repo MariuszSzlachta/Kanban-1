e1b7116a293a250211f1fcd9b958e354eaa4df99
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-06-18 23:20:57 +0200

    Add part of code

--- js/board.js
+++ js/board.js
@@ -2,7 +2,7 @@ var board = {
   name: "Kanban Board", // nazwa
   addColumn: function (column) {
     this.element.appendChild(column.element); // dodaje możliwosc tworzenia kolumn // usówanie bezpośrednio z danej kolumny dlatego w column
-      initSortable(column.id);
+    initSortable(column.id);
   },
   element: document.querySelector('#board .column-container')
 }
@@ -14,17 +14,17 @@ document.querySelector('#board .create-column').addEventListener('click', functi
   data.append('name', name);
 
   fetch(baseUrl + '/column', {
-    method: 'POST',
-    headers: myHeaders,
-    body: data,
-  })
-  .then(function(resp) {
-    return resp.json();
-  })
-  .then(function(resp) {
-    var column = new Column(resp.id, name);
-    board.addColumn(column);
-  });
+      method: 'POST',
+      headers: myHeaders,
+      body: data,
+    })
+    .then(function (resp) {
+      return resp.json();
+    })
+    .then(function (resp) {
+      var column = new Column(resp.id, name);
+      board.addColumn(column);
+    });
 });
 
 function initSortable(id) {
@@ -34,9 +34,39 @@ function initSortable(id) {
     group: 'kanban',
     sort: true,
 
-    receive: function( event, ui ) {
-      console.log(item);
+    onEnd: function (event, ui) {
+      // console.log('id karty:')
+      // console.log(event.item.querySelector('.card').id);
+
+      // console.log('id kolumny')
+      // console.log(id);
+
+      // from i to wskazują na poczatkową ulkę
+      // console.log(event.from.id);
+      // console.log(event.to.id);
+
+      // ulką docelowa
+      // console.log(event.target);
+
+      //id karty którą przeciągam
+      var cardId = event.item.querySelector('.card').id;
+      
+      // id ul-ki docelowej
+      var targetColumnId = event.target.id;
+
+      var data = new FormData();
+        
+      data.append('bootcamp_kanban_column_id', targetColumnId);
+      
+      //wysyłam fetcha w którym aktualizuję id ulki w której znajduje się karta
+      fetch(baseUrl + '/card/' + cardId, {method: 'PUT', headers: myHeaders, body: data })
+        .then(function(resp){
+          return resp.json();
+        })
+        .then(function(resp){
+          // nie wiem czy potrzebuję tego kodu skoro sortable robi za mnie widok
+        })
     }
-    
+
   });
 }
